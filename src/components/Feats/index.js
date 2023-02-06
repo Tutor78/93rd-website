9236a6d6065e6d3e79bba27fb9bc85669db638e7
Tutor <tutor.michael78@gmail.com>
2023-02-06 16:31:11 -0600

    added card component to feats page

--- src/components/Feats/index.js
+++ src/components/Feats/index.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
+import {Card, CardContent, CardMedia, Typography } from '@mui/material';
 
 function Feats() {
     const exploits =[
         {
-            war: '100',
             title:'White Whale in Arcadia',
-            image: 'url(../images/)',
+            image: require('./images/ww-arcadia/salute.jpg'),
             text: `The 93rd participated in the White Whale landing at Arcadia.
                     The landing was organized by the CSU and consisted of 11 regiments.
                     The landing was made up of an armada of around 35 ships of which 2 were
@@ -13,9 +13,17 @@ function Feats() {
                     during fierce combat which lasted over five hours.`
         },
         {
-            war: '100',
             title:'White Whale in Arcadia',
-            image: 'url(../images/)',
+            image: require('./images/ww-arcadia/shelling-one.jpg'),
+            text: `The 93rd participated in the White Whale landing at Arcadia.
+                    The landing was organized by the CSU and consisted of 11 regiments.
+                    The landing was made up of an armada of around 35 ships of which 2 were
+                    from the 93rd. There was a valiant effort by the guns to help break the defenders
+                    during fierce combat which lasted over five hours.`
+        },        
+        {
+            title:'White Whale in Arcadia',
+            image: require('./images/ww-arcadia/shelling-two.jpg'),
             text: `The 93rd participated in the White Whale landing at Arcadia.
                     The landing was organized by the CSU and consisted of 11 regiments.
                     The landing was made up of an armada of around 35 ships of which 2 were
@@ -23,25 +31,35 @@ function Feats() {
                     during fierce combat which lasted over five hours.`
         },
         {
-            war: '100',
             title:'White Whale in Arcadia',
-            image: 'url(../images/)',
+            image: require('./images/ww-arcadia/shelling-night.jpg'),
             text: `The 93rd participated in the White Whale landing at Arcadia.
                     The landing was organized by the CSU and consisted of 11 regiments.
                     The landing was made up of an armada of around 35 ships of which 2 were
                     from the 93rd. There was a valiant effort by the guns to help break the defenders
                     during fierce combat which lasted over five hours.`
-        }
+        },
     ]
 
     return (
-        <div>
-            {exploits.map(({ war, title, image, text }) => (
-                <div>
-                    <h1>War: {war}</h1>
-                    <h2>{title}</h2>
-                    <p>{text}</p>
-                </div>
+        <div style={{alignSelf: 'center'}}>
+            {exploits.map(({ title, image, text }) => (
+                <Card sx={{ maxWidth: 345, margin: 5 }}>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={image}
+                        alt={title}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {text}
+                        </Typography>
+                    </CardContent>
+                </Card>
             ))} 
         </div>
     )
